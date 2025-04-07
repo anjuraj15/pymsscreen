b65cbdc2a7509606ed2d13826d4a3242e28583cf
anjana.elapavalore <anjana.elapavalore@uni.lu>
2025-04-07 21:56:10 +0200

    Add wait-on and backend launch in tests

--- tests/playwright.spec.js
+++ tests/playwright.spec.js
@@ -3,6 +3,10 @@ import { spawn } from 'child_process';
 import path from 'path';
 import fs from 'fs';
 import waitOn from 'wait-on';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 let backendProcess;
 
