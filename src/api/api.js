5b3cc267d10b69038e159042e976893f6fbe0940
anjana.elapavalore <anjana.elapavalore@uni.lu>
2025-04-14 21:29:28 +0200

    fix electron build

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000';
+const API_BASE = 'http://127.0.0.1:5000';
 
 export const saveState = (state) => {
   return axios.post(`${API_BASE}/save_state`, state);
