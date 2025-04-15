36bf15aa4745cd1fe738dc1b14a54e2e4a60e9d0
anjana.elapavalore <anjana.elapavalore@uni.lu>
2025-04-15 17:57:59 +0200

    added backend readiness check

--- web_app.py
+++ web_app.py
@@ -655,7 +655,7 @@ def ping():
     return "pong", 200
 
 if __name__ == '__main__':
-    print("✅ Flask backend starting...")
+    print("Flask backend starting...")
     serve(app, host="127.0.0.1", port=5000)
-    print("🕒 Startup time:", time.time() - start_time, "seconds")
+    print(" Startup time:", time.time() - start_time, "seconds")
     serve(app,host="127.0.0.1", port=5000)
